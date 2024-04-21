3adf7d8c849aab22009cedca825ffc15d24f88e6
chianyungcode <cnytechcode@gmail.com>
2024-04-21 12:57:41 +0700

    feat: add dynamic visibility for table row

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const formElement = document.getElementById("form-address");
 const modalElement = document.getElementById("modal");
 
+// Untuk membuka dan menutup modal
 document.querySelector(".sidebar_button").addEventListener("click", () => {
-  document.getElementById("modal").classList.remove("hidden");
+  modalElement.style.display = "block";
 });
 
-// Menangani klik tombol close untuk menyembunyikan modal
 document.querySelector(".close-button").addEventListener("click", () => {
-  document.getElementById("modal").classList.add("hidden");
+  modalElement.style.display = "none";
 });
 
+// Untuk form submission
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -28,13 +29,29 @@ formElement.addEventListener("submit", (e) => {
 
   localStorage.setItem("contact", JSON.stringify(formData));
 
+  // ! PERLU DIGANTI DENGAN POP UP NOTIFICATION BUKAN ALERT
   alert("This form has been successfully submitted!");
   console.log(
     `This form has a username of ${inputName} and password of ${inputPhone}`
   );
 
+  modalElement.style.display = "none";
+
   inputName = "";
   inputPhone = "";
-
-  document.getElementById("modal").classList.add("hidden");
 });
+
+const storedData = localStorage.getItem("contact");
+
+if (storedData) {
+  const contact = JSON.parse(storedData);
+
+  const tableDataName = document.getElementById("contact-name");
+  const tableDataPhone = document.getElementById("contact-phone");
+
+  tableDataName.textContent = contact.name;
+  tableDataPhone.textContent = contact.phone;
+} else {
+  const tableRow = document.getElementById("table-row");
+  tableRow.style.display = "none";
+}
